bd6816d9a147d5d17578eb50d42d80362fd054e9
Rajdeep Majumder <rajdeepmajumder925@gmail.com>
2020-11-03 19:04:45 +0530

    Add files via upload

---
+++ Guftugu 2.0/spring-boot-WebSocket-chat-app/src/main/java/com/pixeltrice/springbootWebSocketchatapp/GuftuguApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootWebSocketchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuftuguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuftuguApplication.class, args);
+	}
+
+}
